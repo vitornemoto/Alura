23710d69ece9f346e89892af8757a625b1fa9803
Paulo Vitor Silva Nemoto <paulo.nemoto@targetsistemas.com.br>
2019-11-13 13:23:10 -0300

    Finalizando a aula 5

---
+++ AspNetCore/Alura.ListaLeitura.App/HTML/HtmlUtils.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alura.ListaLeitura.App.HTML
+{
+    public class HtmlUtils
+    {
+        public static string CarregaArquivoHtml(string nomeArquivo)
+        {
+            var nomeCompletoArquivo = $"HTML/{nomeArquivo}.html";
+            using (var arquivo = File.OpenText(nomeCompletoArquivo))
+            {
+                return arquivo.ReadToEnd();
+            }
+        }
+    }
+}
